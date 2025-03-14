cc3ff165abedb7132bd83914abbc2506ac8ed211
DevR4yq <100144396+DxRayq@users.noreply.github.com>
2025-03-14 21:20:59 +0100

    Add files via upload

---
+++ fix_pnpm.ps1
@@ -0,0 +1,26 @@
+# Verification of Current Execution Policies
+$currentPolicy = Get-ExecutionPolicy
+
+if ($currentPolicy -eq 'Restricted') {
+    Write-Host "The current script execution policies are set to 'Restricted'. Changing to 'RemoteSigned' for the current user."
+    
+    try {
+        # Changing Execution Policies to RemoteSigned for the Current User
+        Set-ExecutionPolicy RemoteSigned -Scope CurrentUser -Force
+        Write-Host "The script execution policies have been changed to 'RemoteSigned'."
+    } catch {
+        Write-Host "Failed to change the script execution policies. Ensure you have the appropriate permissions."
+        exit
+    }
+} else {
+    Write-Host "The current script execution policies are already set at a level that allows running scripts. No change is needed."
+}
+
+# Running the pnpm inject Command
+try {
+    Write-Host "Running the 'pnpm inject' command..."
+    pnpm inject
+    Write-Host "The 'pnpm inject' command has been executed successfully."
+} catch {
+    Write-Host "An error occurred while running the 'pnpm inject' command."
+}
